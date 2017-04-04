76e052554bee21c0ae38f578ea333283cf54e665
Zaki Manian <zaki@manian.org>
2017-04-04 12:43:01 -0700

    Fixes to typescript exports

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 "use strict";
 exports.__esModule = true;
 var massive = require("massive");
-exports.register = function (server, options, next) {
-    var config = options;
-    massive.connect({
-        connectionString: config.connectionString
-    }, function (err, db) {
-        if (err) {
-            return next(err);
-        }
-        server.expose("db", db);
-        return next();
-    });
-};
-exports.register.attributes = {
-    pkg: require("./package.json")
+exports.plugin = {
+    register: function (server, options, next) {
+        var config = options;
+        massive.connect({
+            connectionString: config.connectionString
+        }, function (err, db) {
+            if (err) {
+                return next(err);
+            }
+            server.expose("db", db);
+            return next();
+        });
+    },
+    attributes: {
+        pkg: require("./package.json")
+    }
 };
--- index.ts
+++ index.ts
@@ -1,25 +1,27 @@
-
 import * as massive from "massive";
 import * as hapi from 'hapi'
 
 
-interface MassiveOptions{
-  connectionString:string
+export interface MassiveOptions {
+  connectionString: string
 }
 
-exports.register = function(server:hapi.Server, options:MassiveOptions, next:Function){
-  const config = options;
+export const plugin = {
+  register: function (server: hapi.Server, options: MassiveOptions, next: Function) {
+    const config = options;
 
-  massive.connect({
-    connectionString: config.connectionString
-  }, function(err, db){
-    if(err){ return next(err); }
+    massive.connect({
+      connectionString: config.connectionString
+    }, function (err, db) {
+      if (err) {
+        return next(err);
+      }
 
-    server.expose("db", db);
-    return next();
-  });
-};
-
-exports.register.attributes = {
-  pkg: require("./package.json")
-};
+      server.expose("db", db);
+      return next();
+    });
+  },
+  attributes: {
+    pkg: require("./package.json")
+  }
+}
