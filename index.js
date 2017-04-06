f29abac29784b64709abe9483ab83a36106f6e23
Zaki Manian <zaki@manian.org>
2017-04-06 16:22:15 -0700

    Hapi massive + models

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 "use strict";
 exports.__esModule = true;
 var massive = require("massive");
+var Path = require("path");
 exports.plugin = {
     register: function (server, options, next) {
-        var config = options;
         massive.connect({
-            connectionString: config.connectionString
+            connectionString: options.connectionString
         }, function (err, db) {
             if (err) {
                 return next(err);
             }
+            var models = options.models || {};
+            // The addModel closure takes a model object and mutates the models variable with new data.
+            // The
+            var addModel = function (key, model, db) {
+                model.db =
+                    models[key] = model;
+                server.expose(key, model);
+            };
+            Object.keys(models).forEach(function (key) {
+                var modelPath = models[key];
+                if (modelPath !== Path.resolve(modelPath)) {
+                    modelPath = Path.join(process.cwd(), modelPath);
+                }
+                addModel(key, require(modelPath), db);
+            });
             server.expose("db", db);
             return next();
         });
--- index.ts
+++ index.ts
@@ -1,21 +1,41 @@
 import * as massive from "massive";
-import * as hapi from 'hapi'
-
+import * as hapi from 'hapi';
+import * as Path from 'path';
 
 export interface MassiveOptions {
   connectionString: string
+  models: any
 }
 
 export const plugin = {
   register: function (server: hapi.Server, options: MassiveOptions, next: Function) {
-    const config = options;
-
     massive.connect({
-      connectionString: config.connectionString
+      connectionString: options.connectionString
     }, function (err, db) {
       if (err) {
         return next(err);
       }
+
+        const models = options.models || {};
+
+        // The addModel closure takes a model object and mutates the models variable with new data.
+        // The
+        const addModel = function (key, model,db) {
+        model.db = 
+        models[key] = model;
+
+        server.expose(key, model);
+      };
+
+      Object.keys(models).forEach((key)=>{
+        let modelPath = models[key];
+
+        if (modelPath !== Path.resolve(modelPath)) {
+            modelPath = Path.join(process.cwd(), modelPath);
+        }
+
+        addModel(key, require(modelPath),db)
+      })
 
       server.expose("db", db);
       return next();
